f225a2574afd5f9a78449860d3fe148d51bf71e5
dyl-baw <dvd.y.lee@gmail.com>
2024-03-08 13:23:01 -0800

    added Tailwindcss is functional now

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,12 +4,10 @@ import SearchBar from "./SearchBar";
 export default function Navbar() {
     return (
         <nav className="nav">
-            <a href="/" className="site-title">MTGTracker</a>
+            <a href="/" class="font-bold text-slate-400 hover: dark-blue">MTGTracker</a>
             <SearchBar />
             <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="/">About Us</a></li>
-                <li><a href="/">Contact Us</a></li>
             </ul>
         </nav>
     );
