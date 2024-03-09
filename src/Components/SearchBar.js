33c9da49ffdab62b951f00ba11d141fa66fa6ec2
Akxn <shen.yen112@gmail.com>
2024-03-08 20:48:29 -0800

    redid search

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { FaSearch } from 'react-icons/fa';
 
 export default function SearchBar() {
+    const [input, setInput] = useState('');
+
+    const fetchDatas = async () => {
+        const response = await fetch(`https://api.scryfall.com/cards/search?q=${input}`);
+        const data = await response.json();
+        console.log(data);
+    }
+
+    const handleSubmit = (value) => {
+        setInput(value);
+        fetchDatas(value);
+    }
+
     return (
         <div className="search">
-            <input type="text" placeholder="Search..." />
+            <input type="text" placeholder="Search..." value={input} 
+            onChange={(e) => handleSubmit(e.target.value)}
+            />
             <button type="submit"><FaSearch /></button>
         </div>
     );
